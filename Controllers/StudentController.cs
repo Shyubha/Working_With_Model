5e77632105d9b45cba992702b4fff0667c549814
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-04-19 12:51:51 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Working_With_Model.Models;
+namespace Working_With_Model.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Students stud)
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(Students stud)
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        { 
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DeleteConform(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Students.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Data.Entity;
+namespace Working_With_Model.Models
+{
+    public class Students
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public int rollno { get; set; }
+
+
+        [DisplayName("Student Name")]
+        [MinLength(2)]
+        [MaxLength(50)]
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Name is required")]
+        public string name { get; set; }
+
+        [Required(ErrorMessage = "Percentage is required")]
+
+        public int percentage { get; set; }
+
+    }
+}
+
